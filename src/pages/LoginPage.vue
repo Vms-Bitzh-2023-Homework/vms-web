28b72b24bf94630efb747bea939c0b71fb4267d6
JulienZeng <936636589@qq.com>
2023-05-06 11:32:53 +0800

    enter 监听

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -14,18 +14,19 @@
                         :model="loginForm"
                     >
                         <el-form-item label="用户名">
-                            <el-input v-model="loginForm.userName" />
+                            <el-input v-model="loginForm.userName" @keyup.enter="login" />
                         </el-form-item>
                         <el-form-item label="密码">
                             <el-input
                                 type="password"
                                 v-model="loginForm.password"
+                                @keyup.enter="login"
                             />
                         </el-form-item>
                     </el-form>
                 </div>
                 <div style="display: flex; justify-content: end">
-                    <el-button type="primary" @click="login">登录</el-button>
+                    <el-button type="primary"  @click="login">登录</el-button>
                 </div>
             </el-card>
         </div>
